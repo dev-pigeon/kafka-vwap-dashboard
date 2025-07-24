b0a78ec0cd6bf96409459c1055679cfa499e433b
Jack Young <jdydev2003@gmail.com>
2025-07-24 15:59:51 -0700

    can open and print file

--- src/main/java/StreamProducer.java
+++ src/main/java/StreamProducer.java
@@ -1,12 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 import org.apache.kafka.clients.producer.*;
 
 public class StreamProducer {
-    private static double rate;
+    private static Long rate = null;
 
     public static void main(String[] args) {
         if (args.length > 0) {
             String rate_string = args[0];
-            rate = Double.parseDouble(rate_string);
+            rate = Long.parseLong(rate_string);
+        }
+        streamFile();
+    }
+
+    private static void streamFile() {
+        String filePath = "./data/stock_data_cleaned.csv";
+        System.out.println("Opening file...");
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            br.readLine(); // dispose of first line
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                Thread.sleep(rate);
+            }
+        } catch (Exception e) {
+
         }
     }
 }
