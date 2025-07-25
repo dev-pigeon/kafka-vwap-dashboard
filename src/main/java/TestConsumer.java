b91f6765537eb8712c1d8c5a77c2b729222a8ea1
Jack Young <jdydev2003@gmail.com>
2025-07-25 16:58:23 -0700

    test consumer echos what is calculated from StreamConsumer

--- src/main/java/TestConsumer.java
+++ src/main/java/TestConsumer.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 
 import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.DoubleDeserializer;
 
 public class TestConsumer {
 
@@ -12,18 +13,18 @@ public class TestConsumer {
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
         props.put("key.deserializer", StringDeserializer.class.getName());
-        props.put("value.deserializer", "StockRecordDeserializer");
+        props.put("value.deserializer", DoubleDeserializer.class.getName());
         props.put("group.id", "kafka-dashboard");
 
-        KafkaConsumer<String, StockRecord> consumer = new KafkaConsumer<>(props);
+        KafkaConsumer<String, Double> consumer = new KafkaConsumer<>(props);
         try {
-            consumer.subscribe(Collections.singletonList("stock-records"));
+            consumer.subscribe(Collections.singletonList("record-VWAPs"));
 
             System.out.println("TestConsumer started...");
             while (true) {
-                ConsumerRecords<String, StockRecord> records = consumer.poll(Duration.ofMillis(100));
-                for (ConsumerRecord<String, StockRecord> record : records) {
-                    System.out.printf("ECHOING: %s", record.toString());
+                ConsumerRecords<String, Double> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, Double> record : records) {
+                    System.out.printf("KEY: %s VALUE: %f", record.key(), record.value());
                 }
             }
         } catch (Exception e) {
