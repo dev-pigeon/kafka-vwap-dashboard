13f39b64adf7a1c22bf28ebadc710f892207e27e
Jack Young <jdydev2003@gmail.com>
2025-07-25 11:47:05 -0700

    added deserializer

---
+++ src/main/java/StockRecordDeserializer.java
@@ -0,0 +1,20 @@
+import org.apache.kafka.common.serialization.Deserializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+
+public class StockRecordDeserializer implements Deserializer<StockRecord> {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public StockRecord deserialize(String topic, byte[] data) {
+        if (data == null) {
+            return null;
+        }
+
+        try {
+            return objectMapper.readValue(data, StockRecord.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Error deserializing MyCustomObject", e);
+        }
+    }
+}
