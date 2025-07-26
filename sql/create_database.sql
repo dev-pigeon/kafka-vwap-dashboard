1039591913247d7182d785ba33e29ee9e0ae1e35
Jack Young <141508421+dev-pigeon@users.noreply.github.com>
2025-07-26 12:53:49 -0700

    added schema (#34)

---
+++ sql/create_database.sql
@@ -0,0 +1,7 @@
+CREATE SCHEMA IF NOT EXISTS kafka_dashboard;
+
+CREATE TABLE IF NOT EXISTS kafka_dashboard.stock_vwap (
+    ticker varchar(255) UNIQUE NOT NULL,
+    vwap float,
+    PRIMARY KEY(ticker)
+);
