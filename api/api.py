8856816b850a9b0e47a6781f2a3fde71ef7ecf50
Jack Young <jdydev2003@gmail.com>
2025-07-26 13:28:10 -0700

    flask server complete

--- api/api.py
+++ api/api.py
@@ -1,4 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
+from db import get_connection
+
+app = Flask(__name__)
+
+
+def get_resources():
+    conn = get_connection()
+    return conn, conn.cursor()
+
+
+def close_resources(conn, cursor):
+    conn.close()
+    cursor.close()
+
+
+@app.route("/stocks", methods=['GET'])
+def get_stocks():
+    conn, cursor = get_resources()
+    cursor.execute("SELECT * FROM kafka_dashboard.stock_vwap;")
+    rows = cursor.fetchall()
+    close_resources(conn, cursor)
+    return jsonify([{"ticker": row[0], "vwap": row[1]} for row in rows])
 
 
 @app.route("/")
