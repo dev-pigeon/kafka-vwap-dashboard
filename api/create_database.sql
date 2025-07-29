31e6ca6ad81ff8d1419a739b34f1b4980a2e57fb
Jack Young <jdydev2003@gmail.com>
2025-07-29 11:22:33 -0700

    updated table schema

--- api/create_database.sql
+++ api/create_database.sql
@@ -3,5 +3,6 @@ CREATE SCHEMA IF NOT EXISTS kafka_dashboard;
 CREATE TABLE IF NOT EXISTS kafka_dashboard.stock_vwap (
     ticker varchar(255) UNIQUE NOT NULL,
     vwap float,
+    last_updated TIMESTAMP WITH TIME ZONE,
     PRIMARY KEY(ticker)
 );
