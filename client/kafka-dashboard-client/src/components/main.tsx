5ee55c9d4016021d1c1b0b8e9bc99009e6efb0ed
Jack Young <jdydev2003@gmail.com>
2025-07-30 10:45:19 -0700

    added basic page

--- client/kafka-dashboard-client/src/components/main.tsx
+++ client/kafka-dashboard-client/src/components/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import "../styles/index.css";
-import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
