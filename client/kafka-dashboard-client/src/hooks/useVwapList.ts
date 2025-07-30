9466170086cfcd5485e01946b619b3020fb6eec7
Jack Young <jdydev2003@gmail.com>
2025-07-30 11:14:09 -0700

    can send request every 20 seconds

---
+++ client/kafka-dashboard-client/src/hooks/useVwapList.ts
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { sendRequest } from "./Api";
+
+const useVwapList = () => {
+    
+    const REQUEST_INTERVAL = 20_000;
+    const url = "http://localhost:5335/top-five";
+
+    const getTopFive = async() => {
+        try {
+            console.log("sending top five request")
+            const topFive = sendRequest(url);
+            console.log(topFive);
+        } catch(error) {
+            if(error instanceof Error) {
+                throw new Error(error.message);
+            }
+        }
+    }
+
+    useEffect(() => {
+        getTopFive();
+        const interval = setInterval(() => {
+            getTopFive();
+        }, REQUEST_INTERVAL);
+        return () => clearInterval(interval);
+    })
+};
+
+export default useVwapList;
