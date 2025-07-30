92d8e6a7e4a08c94b4e3a0b78ecf868e00e14646
Jack Young <jdydev2003@gmail.com>
2025-07-30 11:36:14 -0700

    useVwapList updates state variable

--- client/kafka-dashboard-client/src/hooks/useVwapList.ts
+++ client/kafka-dashboard-client/src/hooks/useVwapList.ts
@@ -1,21 +1,48 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { sendRequest } from "./Api";
+import dayjs, { Dayjs } from "dayjs";
+
+export interface VwapRequestItem {
+    ticker : string,
+    vwap : number,
+    last_updated: string
+}
+
+export interface VwapListItem {
+    ticker : string,
+    vwap : number,
+    last_updated: Dayjs,
+}
 
 const useVwapList = () => {
     
     const REQUEST_INTERVAL = 20_000;
     const url = "http://localhost:5335/top-five";
+    const [vwapList, setVwapList] = useState<VwapListItem[]>([]);
 
     const getTopFive = async() => {
         try {
-            console.log("sending top five request")
-            const topFive = await sendRequest(url);
-            console.log(topFive);
+            const topFiveRequest = await sendRequest<VwapRequestItem[]>(url);
+            const updatedTopFive = processTopFiveResponse(topFiveRequest);
+            setVwapList(updatedTopFive);
         } catch(error) {
             if(error instanceof Error) {
                 throw new Error(error.message);
             }
         }
+    }
+
+    const processTopFiveResponse = (topFiveResponse : VwapRequestItem[]) : VwapListItem[] => {
+        let updatedTopFive : VwapListItem[] = [];
+        for(const requestItem of topFiveResponse) {
+            const listItem : VwapListItem = {
+                ticker : requestItem.ticker,
+                vwap : requestItem.vwap,
+                last_updated : dayjs(requestItem.last_updated),
+            }
+            updatedTopFive.push(listItem);
+        }
+        return updatedTopFive;
     }
 
     useEffect(() => {
@@ -24,7 +51,12 @@ const useVwapList = () => {
             getTopFive();
         }, REQUEST_INTERVAL);
         return () => clearInterval(interval);
-    })
+    },[]);
+
+
+    return {
+        vwapList
+    }
 };
 
 export default useVwapList;
