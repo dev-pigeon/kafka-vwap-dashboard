2a030943dc441366a0fb2d95de207821fb7e1346
Jack Young <jdydev2003@gmail.com>
2025-08-01 10:53:33 -0700

    added Y axis label

--- client/kafka-dashboard-client/src/components/VwapList.tsx
+++ client/kafka-dashboard-client/src/components/VwapList.tsx
@@ -5,16 +5,27 @@ const VwapList = () => {
   const vwapListHook = useVwapList();
 
   return (
-    <BarChart
-      height={window.innerHeight / 2}
-      width={window.innerWidth / 2}
-      dataset={vwapListHook.vwapList}
-      yAxis={[{ scaleType: "band", dataKey: "ticker" }]}
-      series={[{ dataKey: "vwap", label: "Current VWAP", valueFormatter }]}
-      layout="horizontal"
-      grid={{ vertical: true }}
-      colors={["#8454dc"]}
-    />
+    <div>
+      {vwapListHook.vwapList.length > 0 && (
+        <BarChart
+          overflow={"visible"}
+          height={window.innerHeight / 2}
+          width={window.innerWidth / 2}
+          dataset={vwapListHook.vwapList}
+          yAxis={[
+            {
+              scaleType: "band",
+              dataKey: "ticker",
+              label: "Stock Ticker",
+            },
+          ]}
+          xAxis={[{ label: "Volume Weighted Average Price (USD)" }]}
+          series={[{ dataKey: "vwap", label: "Current VWAP", valueFormatter }]}
+          layout="horizontal"
+          colors={["#8454dc"]}
+        />
+      )}
+    </div>
   );
 };
 
