396014f519d34b1cde9eda4adf622e90ff38f48c
Jack Young <jdydev2003@gmail.com>
2025-08-01 10:56:43 -0700

    styled axis labels

--- client/kafka-dashboard-client/src/components/VwapList.tsx
+++ client/kafka-dashboard-client/src/components/VwapList.tsx
@@ -3,6 +3,10 @@ import { BarChart } from "@mui/x-charts";
 
 const VwapList = () => {
   const vwapListHook = useVwapList();
+  const styleLabel = {
+    fontWeight: 500,
+    fontSize: "18px",
+  };
 
   return (
     <div>
@@ -17,9 +21,15 @@ const VwapList = () => {
               scaleType: "band",
               dataKey: "ticker",
               label: "Stock Ticker",
+              labelStyle: styleLabel,
             },
           ]}
-          xAxis={[{ label: "Volume Weighted Average Price (USD)" }]}
+          xAxis={[
+            {
+              label: "Volume Weighted Average Price (USD)",
+              labelStyle: styleLabel,
+            },
+          ]}
           series={[{ dataKey: "vwap", label: "Current VWAP", valueFormatter }]}
           layout="horizontal"
           colors={["#8454dc"]}
