3c0e51cd203b6f2e797a52dc1f48fb6a44fe7fa8
Jack Young <141508421+dev-pigeon@users.noreply.github.com>
2025-08-01 10:12:52 -0700

    Vwap list (#76)
    * can display vwap and ticker in simple list
    
    * rounded vwap to two places

---
+++ client/kafka-dashboard-client/src/components/VwapList.tsx
@@ -0,0 +1,48 @@
+import {
+  Paper,
+  TableContainer,
+  Table,
+  TableRow,
+  TableCell,
+  TableHead,
+  TableBody,
+  tableCellClasses,
+  styled,
+} from "@mui/material";
+import useVwapList from "../hooks/useVwapList";
+
+const VwapList = () => {
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  const vwapListHook = useVwapList();
+  return (
+    <TableContainer sx={{ maxWidth: 300 }} component={Paper}>
+      <Table>
+        {/* header section / column names */}
+        <TableHead>
+          <TableRow>
+            <StyledTableCell sx={{ fontSize: "14px" }}>Ticker</StyledTableCell>
+            <StyledTableCell>VWAP</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {vwapListHook.vwapList.map((value) => (
+            <TableRow key={value.ticker}>
+              <TableCell align="left">{value.ticker}</TableCell>
+              <TableCell align="left">{value.vwap}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default VwapList;
