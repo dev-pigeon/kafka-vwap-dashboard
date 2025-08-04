75c5da19df7817947b3145f6b0733635bf91413e
Jack Young <141508421+dev-pigeon@users.noreply.github.com>
2025-08-04 11:10:04 -0700

    Connection pooling (#89)
    * added hikari cp as dependency
    
    * added hikari datasource
    
    * increased punctuation rate

---
+++ kafka/src/main/java/Database.java
@@ -0,0 +1,25 @@
+import java.sql.SQLException;
+import java.sql.Connection;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class Database {
+    private static final HikariDataSource datasource;
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/kafka_dashboard";
+    private static final String DB_USER = "kafka_user";
+    private static final String DB_PWD = "kafka_password";
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(DB_URL);
+        config.setUsername(DB_USER);
+        config.setPassword(DB_PWD);
+
+        datasource = new HikariDataSource(config);
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return datasource.getConnection();
+    }
+}
