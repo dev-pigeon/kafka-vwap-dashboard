d110970fabea6847afaf92ea27889a86b41e60e4
Jack Young <jdydev2003@gmail.com>
2025-08-18 13:41:19 -0700

    script downloads to correct loccation

--- bin/download_data.sh
+++ bin/download_data.sh
@@ -4,11 +4,11 @@ echo "Downloading dataset..."
 curl -L -o ~/Downloads/9000-tickers-of-stock-market-data-full-history.zip\
   https://www.kaggle.com/api/v1/datasets/download/jakewright/9000-tickers-of-stock-market-data-full-history
 
-mv ~/Downloads/9000-tickers-of-stock-market-data-full-history.zip ~/Documents/Projects/kafka-dashboard/data
-unzip ~/Documents/Projects/kafka-dashboard/data/9000-tickers-of-stock-market-data-full-history.zip 
+mv ~/Downloads/9000-tickers-of-stock-market-data-full-history.zip ~/Documents/Projects/kafka-dashboard/kafka/data
+unzip ~/Documents/Projects/kafka-dashboard/kafka/data/9000-tickers-of-stock-market-data-full-history.zip 
 
-mv ./all_stock_data.csv ../data/
+mv ./all_stock_data.csv ../kafka/data
 rm -rf ../bin/all_stock_data.parquet
-rm -rf ../data/9000-tickers-of-stock-market-data-full-history.zip
+rm -rf ../kafka/data/9000-tickers-of-stock-market-data-full-history.zip
 
 echo "Download complete!"
