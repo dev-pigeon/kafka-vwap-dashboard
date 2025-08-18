6acab46812059a8b99a1407945372ce328d60c12
Jack Young <141508421+dev-pigeon@users.noreply.github.com>
2025-08-18 14:55:12 -0700

    Setup script (#113)
    * added requirements file
    
    * added comprehensive setup script
    
    * removed download script

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-echo "Downloading dataset..."
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 DATA_DIR="$SCRIPT_DIR/../kafka/data"
+
+# ------------- DOWNLOAD DATA -------------
+echo "Downloading zip file..."
 mkdir -p $DATA_DIR
 
 curl -L -o $DATA_DIR/9000-tickers-of-stock-market-data-full-history.zip \
   https://www.kaggle.com/api/v1/datasets/download/jakewright/9000-tickers-of-stock-market-data-full-history
+
+echo "Extracting contents..."
 
 unzip $DATA_DIR/9000-tickers-of-stock-market-data-full-history.zip 
 
@@ -14,3 +18,18 @@ rm -rf $DATA_DIR/9000-tickers-of-stock-market-data-full-history.zip
 mv all_stock_data.csv $DATA_DIR
 
 echo "Download complete!"
+
+
+# ------------- CLEAN DATA -------------
+echo "Initializing Python environment..."
+cd bin
+python3 -m venv venv
+source venv/bin/activate
+pip3 install --no-cache-dir -r requirements.txt
+
+echo "Cleaning dataset..."
+python3 clean_data.py
+rm -rf $DATA_DIR/all_stock_data.csv
+echo "Dataset cleaned!"
+echo "Please run 'docker-compose up --build' to demo the project"
+
