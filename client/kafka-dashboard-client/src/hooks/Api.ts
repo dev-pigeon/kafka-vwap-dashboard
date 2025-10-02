387f20f9711288e5127925b1c40409142166b988
Jack Young <jdydev2003@gmail.com>
2025-10-02 16:06:31 -0700

    removed unused file

--- client/kafka-dashboard-client/src/hooks/Api.ts
+++
@@ -1,5 +0,0 @@
-export async function sendRequest<T=unknown>(url : string) : Promise<T> {
-    const response = await fetch(url);
-    if(!response.ok) throw new Error("Networking response failed");
-    return await response.json();
-}
