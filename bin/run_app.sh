baa6fededda0caf08da04214b5b118ae60354812
Jack Young <jdydev2003@gmail.com>
2025-10-03 10:09:29 -0700

    added dev script

---
+++ bin/run_app.sh
@@ -0,0 +1,13 @@
+NETWORK_NAME="kafka_dashboard"
+
+# check for network
+if docker network inspect "${NETWORK_NAME}" > /dev/null 2>&1; then
+    echo "Docker network '${NETWORK_NAME}' already exists."
+else 
+    echo "Docker network '${NETWORK_NAME}' does not exist. Creating..."
+    docker network create "${NETWORK_NAME}"
+fi
+
+# compose up
+docker compose -f docker-compose.client.yml up -d  
+docker compose -f docker-compose.server.yml up -d 
