31f397570d579cef591db7e4916f74fa2cbdc50b
Jack Young <jdydev2003@gmail.com>
2025-10-06 16:21:46 -0700

    vite env variables

--- client/kafka-dashboard-client/src/hooks/socket.ts
+++ client/kafka-dashboard-client/src/hooks/socket.ts
@@ -1,7 +1,6 @@
 import {io, Socket} from "socket.io-client"
 
 
-const URL = process.env.REACT_APP_API_URL
-export const socket : Socket = io(URL, {
+export const socket : Socket = io(__API_URL__, {
     transports: ['polling','websocket']
 });
