e0b0099f6a8cdd39eb4ddff1beb8abc38b012f02
Jack Young <141508421+dev-pigeon@users.noreply.github.com>
2025-10-06 16:45:14 -0700

    use meta env (#130)

--- client/kafka-dashboard-client/src/hooks/socket.ts
+++ client/kafka-dashboard-client/src/hooks/socket.ts
@@ -1,7 +1,6 @@
 import {io, Socket} from "socket.io-client"
 
-
-const URL = process.env.REACT_APP_API_URL
+const URL = import.meta.env.VITE_API_URL
 export const socket : Socket = io(URL, {
     transports: ['polling','websocket']
 });
